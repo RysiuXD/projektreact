feba83643c9f89e2a67c29c2d0329d487a997abe
RysiuXD <dawid.zelinka@gmail.com>
2023-06-14 18:45:12 +0200

    path

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react'
-import {BrowserRouter, Route} from 'react-router-dom'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import HomePage from './pages/HomePage/HomePage'
+import ActorPage from './pages/ActorPage/ActorPage'
+import Details from './pages/Details/Details'
+import Search from './pages/Search/Search'
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<HomePage/>}/>
-        <Route path="/actorPage" element={<ActorPage/>}/>
-        <Route path="/details" element={<Details/>}/>
+        <Route path="/actorPage/:name" element={<ActorPage/>}/>
+        <Route path="/details/:id" element={<Details/>}/>
         <Route path="/search" element={<Search/>}/>
 
       </Routes>
