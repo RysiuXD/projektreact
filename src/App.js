218ad6f49645fd29b8793c3a82f7a89a36c80a7d
RysiuXD <dawid.zelinka@gmail.com>
2023-06-14 18:17:24 +0200

    strony

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import {BrowserRouter, Route} from 'react-router-dom'
+import HomePage from './pages/HomePage/HomePage'
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>}/>
+        <Route path="/actorPage" element={<ActorPage/>}/>
+        <Route path="/details" element={<Details/>}/>
+        <Route path="/search" element={<Search/>}/>
+
+      </Routes>
+    </BrowserRouter>
   )
 }
 
